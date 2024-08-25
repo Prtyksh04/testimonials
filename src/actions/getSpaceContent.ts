cf0b5989730f348e6fdcd80e077bacfe0cced068
pratyaksh <pratyaksh22csu447@ncuindia.edu>
2024-08-25 17:46:26 +0530

    resolved some error

---
+++ src/actions/getSpaceContent.ts
@@ -0,0 +1,20 @@
+import prisma from "@/db";
+export async function getSpaceContent(spaceName: string) {
+    try {
+        const spaceContent = await prisma.space.findFirst({
+            where: {
+                spaceName: spaceName
+            },
+            select: {
+                headerTitle: true,
+                customMessage: true,
+                questions: true,
+            }
+        });
+        return spaceContent || { headerTitle: '', customMessage: '', questions: [] }; 
+    } catch (error) {
+        console.error("Error fetching Space Content:", error);
+        return { headerTitle: '', customMessage: '', questions: [] };
+    }
+}
+
--- src/app/[space]/page.tsx
+++ src/app/[space]/page.tsx
@@ -1,28 +1,5 @@
 import TestimonialPage from "@/app/Components/TestimonialSpacePage/TestimonialSpacePage";
-import prisma from "@/db";
-
-export async function getSpaceContent(spaceName: string) {
-    try {
-        const spaceContent = await prisma.space.findFirst({
-            where: {
-                spaceName: spaceName
-            },
-            select: {
-                headerTitle: true,
-                customMessage: true,
-                questions: true,
-            }
-        });
-        return spaceContent || { headerTitle: '', customMessage: '', questions: [] }; 
-    } catch (error) {
-        console.error("Error fetching Space Content:", error);
-        return { headerTitle: '', customMessage: '', questions: [] };
-    }
-}
-
-
-
-
+import { getSpaceContent } from "@/actions/spaces";
 export default async function Page({ params }: { params: { space: string } }) {
     const spaceContent = await getSpaceContent(params.space);
     return (
