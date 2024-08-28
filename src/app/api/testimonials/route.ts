a304eea5eacdef66c4f83685ebe4f015b363037d
pratyaksh <pratyaksh22csu447@ncuindia.edu>
2024-08-28 22:00:50 +0530

    changed clerkmiddleware

--- src/app/api/testimonials/route.ts
+++ src/app/api/testimonials/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from '@/db';
+import prisma from '@/db'; // Adjust the path as necessary
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
@@ -27,8 +27,8 @@ export async function GET(request: NextRequest) {
                 content: true,
                 submittedAt: true,
                 videoUrl: true,
-                id: true
-            }
+                id: true,
+            },
         });
 
         return new NextResponse(JSON.stringify(testimonials), {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,20 @@
-import { clerkMiddleware , createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)' , '/test(.*)' , '/' , '/api/testimonials(.*)' , '/testing/test']);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/test(.*)', '/', '/api/testimonials(.*)', '/testing/test']);
 
 export default clerkMiddleware((auth, request) => {
-  console.log('Request URL:', request.nextUrl.pathname);
+  const response = NextResponse.next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (!isPublicRoute(request)) {
-      console.log('Protected route, requiring authentication.');
-      auth().protect();
+    console.log('Protected route, requiring authentication.');
+    auth().protect();
   } else {
-      console.log('Public route, no authentication required.');
+    console.log('Public route, no authentication required.');
   }
+  return response;
 });
 
 
