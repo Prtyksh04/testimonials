35aeb36443320e3122547804781dc82ba1110e40
pratyaksh <pratyaksh22csu447@ncuindia.edu>
2024-09-01 22:39:46 +0530

    callback url changes

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -19,10 +19,9 @@ export const AuthOptions: NextAuthOptions = ({
   },
   callbacks: {
     async redirect({ url, baseUrl }) {
-      if (url.startsWith(baseUrl)) {
-        return url;
-      }
-      return baseUrl + '/dashboard';
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+    else if (new URL(url).origin === baseUrl) return url
+    return baseUrl
     },
     async signIn(params) {
       if (!params.user.email) {
