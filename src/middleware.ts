fa113e3ad7ef96d063dd4666f98e5975ab041807
pratyaksh <pratyaksh22csu447@ncuindia.edu>
2024-09-01 23:32:46 +0530

    fixed middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// Secret used to sign the JWT, it should match the one in your [...nextauth].ts file
+const secret = process.env.NEXTAUTH_SECRET;
 
 export async function middleware(req: NextRequest) {
-  // Retrieve the token from the request
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({ req, secret });
+
   const { pathname } = req.nextUrl;
 
-  // Redirect to sign-in page if not authenticated and accessing a protected page
-  if (pathname.startsWith('/dashboard') || pathname.startsWith('/products')) {
-    if (!token) {
-      return NextResponse.redirect(new URL('/auth/signin', req.url));
-    }
+  if ((pathname.startsWith("/dashboard") || pathname.startsWith("/products")) && !token ) {
+    return NextResponse.redirect(new URL("/auth/signin", req.url));
   }
 
-  // If authenticated and accessing sign-in page, redirect to dashboard
-  if (pathname === '/auth/signin' && token) {
-    return NextResponse.redirect(new URL('/dashboard', req.url));
+  if (pathname === "/auth/signin" && token) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
-
   return NextResponse.next();
 }
 
+// Match the routes you want to protect
 export const config = {
-  matcher: ['/dashboard/:path*', '/products/:path*']
+  matcher: ["/dashboard/:path*", "/products/:path*"],
 };
