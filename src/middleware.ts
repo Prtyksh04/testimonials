2bfa63ed64daa133910706c102680e978c6d6dd5
pratyaksh <pratyaksh22csu447@ncuindia.edu>
2024-09-02 11:21:30 +0530

    push for debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,13 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
-// Secret used to sign the JWT, it should match the one in your [...nextauth].ts file
 const secret = process.env.NEXTAUTH_SECRET;
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret });
 
   const { pathname } = req.nextUrl;
+  console.log("Token : ", token);
 
   if ((pathname.startsWith("/dashboard") || pathname.startsWith("/products")) && !token ) {
     return NextResponse.redirect(new URL("/auth/signin", req.url));
